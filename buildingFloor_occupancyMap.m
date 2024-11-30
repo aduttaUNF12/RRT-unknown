21eabcfe024ecee33d70edbb0352757f87495a83
Irene-Ramirez <n01512644@unf.edu>
2024-11-30 14:04:48 -0500

    Add files via upload
    10 by 10 occupancy map worked on the base program for RRT* with a normal amount of numNodes, the save map option is included in the code.

---
+++ buildingFloor_occupancyMap.m
@@ -0,0 +1,23 @@
+clearvars
+close all
+
+img = imread('buildingFloor.png');
+
+% Convert to grayscale if the image is RGB
+if size(img, 3) == 3
+    img = rgb2gray(img);
+end
+
+% Convert to binary and invert (black objects as occupied)
+bwImg = imbinarize(img);
+
+% Resize to high resolution
+highResMap = imresize(bwImg, [1000, 1000], 'nearest'); % High resolution 
+
+% Create the binary occupancy map with a physical size of 10x10
+buildingFloorMap = binaryOccupancyMap(highResMap, 100); % 100 cells per unit size
+save("gridmaps.mat","buildingFloorMap");
+% Display the final map
+figure;
+show(buildingFloorMap);
+hold on;
